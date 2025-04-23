3b79f659f0df3e41605c167aaa5b957796ea6347
Carlos España <carlin.08.12@gmail.com>
2025-04-22 23:45:50 -0500

    Initial Commit

---
+++ MoviesRimac/MoviesRimacApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesRimacApp.swift
+//  MoviesRimac
+//
+//  Created by Carlos on 22/04/25.
+//
+
+import SwiftUI
+
+@main
+struct MoviesRimacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
